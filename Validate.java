946cd31add3b9fcf79bc2e6a2a1748d94496d0f0
yifeizhao <yf295016@dal.ca>
2018-06-09 07:00:14 -0300

    second commit

--- Validate.java
+++ Validate.java
@@ -26,8 +26,37 @@ public class Validate {
             return 1;
         }
     }
+    public int special(){
+        if(password.contains("-")){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
+
+
+    public int UpLow(){
+        if(password.equals(password.toLowerCase()) || password.equals(password.toUpperCase())){
+            return 0;
+        }
+        else{
+            return 1;
+        }
+    }
+
+
+    public int digit(){
+        if(password.matches(".*\\d+.*")){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
+
 
     public int numberPassed(){
-        return same()+length();
+        return same()+length()+special()+UpLow()+digit();
     }
 }
